e9836cfa1b0a4a1342393c485bb721264b1e95d0
yujin37 <yj061628@gmail.com>
2025-10-31 12:26:49 +0900

    test: 로또 번호 중복, 크기, 정렬 테스트 코드 추가

--- src/test/java/lotto/LottoNumberTest.java
+++ src/test/java/lotto/LottoNumberTest.java
@@ -1,8 +1,38 @@
 package lotto;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.domain.LottoMachine;
+import org.junit.jupiter.api.Test;
+
 public class LottoNumberTest {
     //로또 번호가 제대로 나왔는지 검증
     //1.중복이 있는가
+    @Test
+    void 중복이_있는가() {
+        LottoMachine lottoMachine = new LottoMachine();
+        List<Lotto> tickets = lottoMachine.issueTicket(1);
+        List<Integer> ticket = tickets.get(0).getNumbers();
+        long duplicateCount = ticket.stream().distinct().count();
+        assertEquals(6, duplicateCount);
+    }
     //2.6개로 제대로 나왔는가
+    @Test
+    void 지정한_로또_크기만큼_나왔는가() {
+        LottoMachine lottoMachine = new LottoMachine();
+        List<Lotto> tickets = lottoMachine.issueTicket(1);
+        List<Integer> ticket = tickets.get(0).getNumbers();
+        assertEquals(6, ticket.size());
+    }
     //3.정렬이 되어있는가
+    @Test
+    void 정렬이_되었는가() {
+        LottoMachine lottoMachine = new LottoMachine();
+        List<Lotto> tickets = lottoMachine.issueTicket(1);
+        List<Integer> ticket = tickets.get(0).getNumbers();
+        assertThat(ticket).isSorted();
+    }
 }
