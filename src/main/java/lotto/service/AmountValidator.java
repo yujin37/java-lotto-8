e789dec758f8edb8d500bf142535aa6b4587c87e
yujin37 <yj061628@gmail.com>
2025-10-31 11:11:25 +0900

    feat: 구매 금액 검증 로직에 최소, 최대 금액 추가

--- src/main/java/lotto/service/AmountValidator.java
+++ src/main/java/lotto/service/AmountValidator.java
@@ -11,14 +11,29 @@ public class AmountValidator {
         }
         return money;
     }
+
     public static void isUnit(int money) {
         if(money % 1000 != 0) {
             throw new IllegalArgumentException("[Error] 구매금액이 1000으로 나누어 떨어지지 않습니다. ");
         }
     }
+
+    public static void isNegative(int money) {
+        if(money < 1000) {
+            throw new IllegalArgumentException("[Error] 구매금액은 1000원 이상이어야 합니다.");
+        }
+    }
+
+    public static void isMaximum(int money) {
+        if(money > 100000) {
+            throw new IllegalArgumentException("[Error] 로또를 한번에 살 수 있는 구매 금액은 최대 10만원 입니다.");
+        }
+    }
+
     public static void isAmount(String amount) {
         int money = isNumber(amount);
         isUnit(money);
-
+        isNegative(money);
+        isMaximum(money);
     }
 }
