6fb6c29eeafdc09e4f646ec4fa35cbcb44527f6d
yujin37 <yj061628@gmail.com>
2025-10-31 19:07:32 +0900

    feat: 보너스 번호 당첨번호 중복 처리 및 범위 검사 기능 추가

--- src/main/java/lotto/exception/NumberErrorMessage.java
+++ src/main/java/lotto/exception/NumberErrorMessage.java
@@ -5,8 +5,8 @@ public enum NumberErrorMessage {
     WINNING_NUMBERS_NOT_NUMBER("[ERROR] 당첨 번호가 숫자가 아닙니다."),
     BONUS_NUMBER_NOT_NUMBER("[ERROR] 보너스 번호가 숫자가 아닙니다."),
     BONUS_NUMBER_EMPTY("[ERROR] 보너스 번호가 비어있습니다."),
-    BONUS_DUPLICATE_WINNING_NUMBER("[ERROR] 보너스 번호가 당첨 번호와 중복됩니다.");
-
+    BONUS_DUPLICATE_WINNING_NUMBER("[ERROR] 보너스 번호가 당첨 번호와 중복됩니다."),
+    BONUS_NUMBER_NOT_RANGE("[ERROR] 보너스 번호가 범위에 속하지 않습니다. 보너스 번호는 1 ~ 45 사이의 숫자여야 합니다.");
     private final String message;
 
     NumberErrorMessage(String message) {
--- src/main/java/lotto/service/BonusValidator.java
+++ src/main/java/lotto/service/BonusValidator.java
@@ -4,9 +4,19 @@ import java.util.List;
 import lotto.exception.NumberErrorMessage;
 
 public class BonusValidator {
-    public static void validateBonus(List<Integer> winningNumber, int bonusNumber) {
+    private static void duplicateBonus(List<Integer> winningNumber, int bonusNumber) {
         if(winningNumber.contains(bonusNumber)) {
             throw new IllegalArgumentException(NumberErrorMessage.BONUS_DUPLICATE_WINNING_NUMBER.getMessage());
         }
+    }
+
+    private static void validateRange(int bonusNumber) {
+        if(bonusNumber < 1 || bonusNumber > 45) {
+            throw new IllegalArgumentException(NumberErrorMessage.BONUS_NUMBER_NOT_RANGE.getMessage());
+        }
+    }
+    public static void validateBonus(List<Integer> winningNumber, int bonusNumber) {
+        duplicateBonus(winningNumber, bonusNumber);
+        validateRange(bonusNumber);
     }
 }
