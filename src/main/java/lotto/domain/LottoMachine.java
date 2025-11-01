cbdb170228c865406c7ea2afee060978e6b49cbb
yujin37 <yj061628@gmail.com>
2025-11-01 19:59:20 +0900

    fix: 로또 발행 후 번호 정렬 수정

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -1,18 +1,18 @@
 package lotto.domain;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class LottoMachine {
     public List<List<Integer>> issueTicket(int count) {
 
         List<List<Integer>> tickets = new ArrayList<>();
-        for(int i = 0; i < count ; i++) {
+        for (int i = 0; i < count; i++) {
             List<Integer> numbers = GenerateNumbers.LottoNumbers();
-            Collections.sort(numbers);
-            tickets.add(numbers);
+            List<Integer> sortedNumbers = numbers.stream()
+                    .sorted()
+                    .toList();
+            tickets.add(sortedNumbers);
         }
         return tickets;
     }
