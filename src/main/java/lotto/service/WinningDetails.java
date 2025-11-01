456d0a80e6b0b005b966bff89729ec51a9af9723
yujin37 <yj061628@gmail.com>
2025-11-01 22:51:25 +0900

    refactor: 5개 일치 로직 삼항 연산자 변환 및 스타일 변경

--- src/main/java/lotto/service/WinningDetails.java
+++ src/main/java/lotto/service/WinningDetails.java
@@ -1,6 +1,5 @@
 package lotto.service;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Predicate;
@@ -8,43 +7,40 @@ import lotto.domain.Rank;
 
 public class WinningDetails {
     public Map<Rank, Integer> calculateWinning(List<List<Integer>> tickets, List<Integer> winningNumbers, int bonus) {
-        Map<Rank, Integer> resultWinning = new HashMap<>(Map.of(
-                Rank.FIRST, 0,
-                Rank.SECOND, 0,
-                Rank.THIRD, 0,
-                Rank.FOURTH, 0,
-                Rank.FIFTH, 0,
-                Rank.LOSE, 0
-        ));
-        for(List<Integer> ticket : tickets) {
+        Map<Rank, Integer> resultWinning = WinningResultFactory.createInitialResult();
+        for (List<Integer> ticket : tickets) {
             long correctValue = guessNumbers(ticket, winningNumbers);
             boolean hasBonus = guessBonus(ticket, bonus);
             Rank rank = guessRank(correctValue, hasBonus);
-            resultWinning.put(rank, resultWinning.get(rank)+1);
+            resultWinning.put(rank, resultWinning.get(rank) + 1);
         }
         return resultWinning;
     }
+
     private long guessNumbers(List<Integer> ticket, List<Integer> winningNumbers) {
         return ticket.stream()
                 .filter(t -> winningNumbers.stream()
                         .anyMatch(Predicate.isEqual(t)))
                 .count();
     }
+    
     private boolean guessBonus(List<Integer> ticket, int bonus) {
         return ticket.contains(bonus);
     }
 
     private Rank guessRank(long correct, boolean hasBonus) {
-        if(correct == 3) return Rank.FIFTH;
-        if(correct == 4) return Rank.FOURTH;
-        if(correct == 5 && hasBonus == false) {
-            return Rank.THIRD;
+        if (correct == 3) {
+            return Rank.FIFTH;
         }
-        if(correct == 5 && hasBonus == true) {
-            return Rank.SECOND;
+        if (correct == 4) {
+            return Rank.FOURTH;
         }
-        if(correct == 6) return Rank.FIRST;
-
+        if (correct == 5) {
+            return hasBonus ? Rank.THIRD : Rank.SECOND;
+        }
+        if (correct == 6) {
+            return Rank.FIRST;
+        }
         return Rank.LOSE;
     }
 }
