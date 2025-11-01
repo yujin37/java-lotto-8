19151979549545d856115cb6f1f61b21c9be51e6
yujin37 <yj061628@gmail.com>
2025-11-01 18:56:37 +0900

    style: 에러메시지 시작 부분 대문자로 변경

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -20,7 +20,7 @@ public class Lotto {
 
     private void duplicate(List<Integer> numbers) {
         if (numbers.size() != numbers.stream().distinct().count()) {
-            throw new IllegalArgumentException("[Error] 중복된 번호가 있습니다.");
+            throw new IllegalArgumentException("[ERROR] 중복된 번호가 있습니다.");
         }
     }
 
@@ -29,7 +29,7 @@ public class Lotto {
                 .filter(n -> (n >= 0 && n <= 45))
                 .count();
         if (filteredNumbers != numbers.size()) {
-            throw new IllegalArgumentException("[Error] 로또 번호가 범위에 있지 않습니다. 반드시 1 ~ 45 사이의 숫자로 구성되어야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 로또 번호가 범위에 있지 않습니다. 반드시 1 ~ 45 사이의 숫자로 구성되어야 합니다.");
         }
     }
 
--- src/main/java/lotto/exception/AmountErrorMessage.java
+++ src/main/java/lotto/exception/AmountErrorMessage.java
@@ -1,10 +1,10 @@
 package lotto.exception;
 
 public enum AmountErrorMessage {
-    AMOUNT_NOT_NUMBER("[Error] 구매 금액이 숫자가 아닙니다."),
-    AMOUNT_NOT_DIVIDE_UNIT("[Error] 구매금액이 1000으로 나누어 떨어지지 않습니다."),
-    AMOUNT_MIN_MORE("[Error] 구매금액은 1000원 이상이어야 합니다."),
-    AMOUNT_MAX_UNDER("[Error] 로또를 한번에 살 수 있는 구매 금액은 최대 10만원 입니다.");
+    AMOUNT_NOT_NUMBER("[ERROR] 구매 금액이 숫자가 아닙니다."),
+    AMOUNT_NOT_DIVIDE_UNIT("[ERROR] 구매금액이 1000으로 나누어 떨어지지 않습니다."),
+    AMOUNT_MIN_MORE("[ERROR] 구매금액은 1000원 이상이어야 합니다."),
+    AMOUNT_MAX_UNDER("[ERROR] 로또를 한번에 살 수 있는 구매 금액은 최대 10만원 입니다.");
 
     private final String message;
 
