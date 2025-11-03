d1bd29a2b3d5b8b5af1b8a8b6b9eab884a6d2e98
yujin37 <yj061628@gmail.com>
2025-11-03 10:39:16 +0900

    test: 구매 금액 입력 함수 통합 테스트 코드 작성

---
+++ src/test/java/lotto/AmountUnitTest.java
@@ -0,0 +1,116 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import lotto.exception.LottoMessage;
+import lotto.service.LottoInputService;
+import lotto.util.CountConverter;
+import lotto.util.NumbersConverter;
+import lotto.view.InputView;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+public class AmountUnitTest {
+
+    @AfterEach
+    void afterEach() {
+        Console.close(); // 리소스 해제
+    }
+
+    @Test
+    void 구매금액_정상입력인_경우() {
+        //given
+        String amountInput = "2000\n";
+        System.setIn(new ByteArrayInputStream(amountInput.getBytes()));
+
+        InputView inputView = new InputView();
+
+        LottoInputService service = new LottoInputService(
+                inputView,
+                new CountConverter(),
+                new NumbersConverter()
+        );
+
+        //when
+        int amount = service.checkAmount();
+
+        //then
+        assertThat(amount).isEqualTo(2);
+    }
+
+    @Test
+    void 구매금액_범위가_아닌경우_에러발생() {
+        //given
+        String amountInput = "-2000\n100\n101000\n1000\n";
+        System.setIn(new ByteArrayInputStream(amountInput.getBytes()));
+
+        InputView inputView = new InputView();
+        LottoInputService service = new LottoInputService(
+                inputView,
+                new CountConverter(),
+                new NumbersConverter()
+        );
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        //when
+        service.checkAmount();
+
+        //then
+        assertThat(out.toString()).contains("[ERROR]");
+        assertThat(out.toString()).contains(LottoMessage.PURCHASE_MESSAGE);
+    }
+
+    @Test
+    void 구매금액_문자가_들어온경우_에러발생() {
+        //given
+        String amountInput = "2000!\n2000a\n2000원\n2000\n";
+        System.setIn(new ByteArrayInputStream(amountInput.getBytes()));
+
+        InputView inputView = new InputView();
+        LottoInputService service = new LottoInputService(
+                inputView,
+                new CountConverter(),
+                new NumbersConverter()
+        );
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        //when
+        service.checkAmount();
+
+        //then
+        assertThat(out.toString()).contains("[ERROR]");
+        assertThat(out.toString()).contains(LottoMessage.PURCHASE_MESSAGE);
+    }
+
+    @Test
+    void 구매금액_빈_문자열시_에러발생() {
+        //given
+        String amountInput = "\n \n2000";
+        System.setIn(new ByteArrayInputStream(amountInput.getBytes()));
+
+        InputView inputView = new InputView();
+        LottoInputService service = new LottoInputService(
+                inputView,
+                new CountConverter(),
+                new NumbersConverter()
+        );
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        //when
+        service.checkAmount();
+
+        //then
+        assertThat(out.toString()).contains("[ERROR]");
+        assertThat(out.toString()).contains(LottoMessage.PURCHASE_MESSAGE);
+    }
+}
